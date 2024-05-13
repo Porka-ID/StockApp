02cdb1803281fe08b5fa4b8fb048cc300900a71a
Porka <kfougere@francoisdesales.be>
2024-05-13 14:23:09 +0200

    sss
    s

--- application/db.py
+++ application/db.py
@@ -8,3 +8,7 @@ class Database:
 
     def getAll(self):
         return self.col.find() or False
+    
+    def insertStock(self, name, type, nbrStock, infos):
+        self.col.insert_one({"name": name, "type": type, "qty": nbrStock, "infos": infos})
+        return name, type, nbrStock, infos
--- application/index.py
+++ application/index.py
@@ -12,20 +12,100 @@ import db as db
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
-
-class insertStockWin(customtkinter.CTk):
-    def __init__(self) -> None:
-        super().__init__()
+class popupWin(customtkinter.CTkToplevel):
+    def __init__(self, parent, title, geometry="550x250") -> None:
+        super().__init__(parent)
 
 
         self.on_closing = None
-        self.title("Inserer un nouvel élement")
-        self.geometry("550x250")
+        self.title(title)
+        self.geometry(geometry)
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.isOpen = False
+        
+    def on_closing(self):
+        self._window_exists = False
+        self.destroy()
 
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+class insertStockWin(popupWin):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, "Inserer un élement", geometry='500x350', **kwargs)
+        self.infos = {}
+        self.parent = parent
+
+        # Nom de l'élement widget
+        self.strName = customtkinter.StringVar()
+        self.labelName = customtkinter.CTkLabel(self, text="Nom de l'élement", anchor="w", justify="left")
+        self.labelName.grid(row=0, column=0, sticky="w", padx=30, pady=(20, 0), ipadx=10)
+        self.inputName = customtkinter.CTkEntry(self, placeholder_text="Nom")
+        self.inputName.grid(row=1, column=0, sticky="w", padx=28, ipadx=10)
+
+        #Type de l'élement widget
+        self.labelType = customtkinter.CTkLabel(self, text="Type de l'élement", anchor="w", justify="left")
+        self.labelType.grid(row=0, column=1, sticky="w", padx=(30, 0), pady=(20, 0), ipadx=10)
+        self.inputType = customtkinter.CTkComboBox(self, values=["Nourriture", "Boisson"])
+        self.inputType.grid(row=1, column=1, sticky="w", padx=28, ipadx=10)
+
+        #Stock déja présent de l'élement widget
+        self.labelStock = customtkinter.CTkLabel(self, text="Stock de l'élement", anchor="w", justify="left")
+        self.labelStock.grid(row=3, column=0, sticky="w", padx=30, pady=(20, 0), ipadx=10)
+        self.inputStock = customtkinter.CTkEntry(self, placeholder_text="number")
+        self.inputStock.grid(row=4, column=0, sticky="w", padx=28, ipadx=10)
+        
+        #Information de l'élement
+        self.strInfoKey = customtkinter.StringVar()
+        self.labelInfos = customtkinter.CTkLabel(self, text="Information(s) du produit", anchor='w', justify='left')
+        self.labelInfos.grid(row=3, column=1, sticky="w", padx=30, pady=(20, 0), ipadx=10)
+        self.inputInfoKey = customtkinter.CTkEntry(self, placeholder_text="Clé de l'info", textvariable=self.strInfoKey)
+        self.inputInfoKey.grid(row=4, column=1, sticky='w', padx=28, ipadx=10)
+        self.strInfoGet = customtkinter.StringVar()
+        self.inputInfoGet = customtkinter.CTkEntry(self, placeholder_text="Info", textvariable=self.strInfoGet)
+        self.inputInfoGet.grid(row=5, column=1, sticky='w', padx=28, pady=(5, 0), ipadx=10)
+
+        #Ajouter une info
+        self.btnAdd = customtkinter.CTkButton(self, width=50, height=20, text='Ajouter', fg_color="#373737", hover_color='#414141', command=self.addToStock) #command=lambda: self.addInfo(self.strInfoKey.get(), self.strInfoGet.get()) )
+        self.btnAdd.grid(row=5, column=1, sticky='e', padx=(0, 15), pady=(5, 0))
+
+        #Listing des infos ajoutées
+        columns = ('key', 'info')
+        self.table = ttk.Treeview(master=self, columns=columns, height=5, selectmode='browse', show='headings')
+        self.table.column("#1", anchor='c', width=25, minwidth=25)
+        self.table.column('#2', anchor='w', minwidth=25)
+        # =============+ Table head +==============
+        self.table.heading('key', text='Clé')
+        self.table.heading('info', text="Info")
+        self.table.grid(row=6, column=1, sticky='nsew', padx=20, pady=10)
+        
+    def addInfo(self, key, info):
+        if not key or not info:
+            print("Erreur")
+            return
+        try: 
+            if self.infos[key]:
+                print("same")
+        except:
+            self.infos[key] = info
+            self.table.insert(parent='', index='end', text='', values=(key, info))
+            
+    def addToStock(self):
+        name = self.inputName.get()
+        type = self.inputType.get()
+        try:
+            nbrStock = int(self.inputStock.get())
+        except:
+            nbrStock = False
+            
+        self.infos = self.infos
+        
+        if len(name) > 1 and nbrStock:
+            print(name, type, nbrStock, self.infos)
+            print(self.parent.master.frameLeft.connectDb.insertStock(name, type, nbrStock, self.infos))
+        else:
+            print("Il manque des infos")
+
+
+
 
 class buttonStock(customtkinter.CTkButton):
     def __init__(self, master, **kwargs):
@@ -34,29 +114,30 @@ class buttonStock(customtkinter.CTkButton):
 class BtnFrame(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.master = master
 
-        self.button1 = buttonStock(self, text="Ajouter un\nélement", command=self.button_event)
+        self.button1 = buttonStock(self, text="Ajouter un\nélement", command=self.insertFrameView)
         self.button1.grid(row=0, column=0, padx=10, pady=10)
 
-        self.button2 = buttonStock(self, text="Retirer du\nstock à un\nélement", command=self.button_event)
+        self.button2 = buttonStock(self, text="Retirer du\nstock à un\nélement", command=self.insertFrameView)
         self.button2.grid(row=1, column=0, padx=10, pady=10)
 
-        self.button3 = buttonStock(self, text="Retirer un\nélement", command=self.button_event)
+        self.button3 = buttonStock(self, text="Retirer un\nélement", command=self.insertFrameView)
         self.button3.grid(row=2, column=0, padx=10, pady=10)
 
-        self.button4 = buttonStock(self, text="Supprimer \ntous les\nélements", command=self.button_event)
+        self.button4 = buttonStock(self, text="Supprimer \ntous les\nélements", command=self.insertFrameView)
         self.button4.grid(row=3, column=0, padx=10, pady=10)
 
-        self.button5 = buttonStock(self, text="Ajouter du\nstock à un\nélementt", command=self.button_event)
+        self.button5 = buttonStock(self, text="Ajouter du\nstock à un\nélementt", command=self.insertFrameView)
         self.button5.grid(row=0, column=1, padx=10, pady=10)
 
-        self.button6 = buttonStock(self, text="Retirer du\nstock à un\nélement", command=self.button_event)
+        self.button6 = buttonStock(self, text="Retirer du\nstock à un\nélement", command=self.insertFrameView)
         self.button6.grid(row=1, column=1, padx=10, pady=10)
         
 
-    def button_event(self):
-        self.newWin = insertStockWin()
-        self.newWin.mainloop()
+    def insertFrameView(self):
+        self.newWin = insertStockWin(self)
+        self.newWin.grab_set()
 
 
 # =============+ View Table of Stock +==============
@@ -128,13 +209,13 @@ class StockApp(customtkinter.CTk):
 
         self.frameLeft = StockViewFrame(master=self, corner_radius=15, height=540, width=600)
         self.frameLeft.grid(row=0, column=0, padx=20, pady=20, sticky='nsew')
-        self.frameLeft = BtnFrame(master=self, corner_radius=15, height=540, width=260)
-        self.frameLeft.grid(row=0, column=1, padx=20, pady=20, sticky='nsew')
+        self.frameRight = BtnFrame(master=self, corner_radius=15, height=540, width=260)
+        self.frameRight.grid(row=0, column=1, padx=20, pady=20, sticky='nsew')
 
+        self.mainloop()
     
         
 
 
 if __name__ == "__main__":
     app = StockApp()
-    app.mainloop()
